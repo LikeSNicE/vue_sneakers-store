f2a0ec2602c61abbf9f10bb8e98a797785a26436
Nikita Lutsenko <nik.luzenko@mail.ru>
2025-12-17 15:57:26 +0500

    добавил измение к билду 17-12-2025

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import Favorites from '@/pages/Favorites.vue'
 import AuthPage from '@/pages/AuthPage.vue'
@@ -69,7 +69,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
